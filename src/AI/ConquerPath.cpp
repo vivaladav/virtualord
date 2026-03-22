861ea1d29a7f3fcbb2fc5efe601fc3b708203afb
Davide Coppola <vivaladav@gmail.com>
2026-03-22 23:43:52 +0100

    Improve accuracy of PathEnergyCost of ConquerPath

--- src/AI/ConquerPath.cpp
+++ src/AI/ConquerPath.cpp
@@ -337,10 +337,19 @@ void ConquerPath::UpdateMove(float delta)
 
 void ConquerPath::UpdatePathCost()
 {
-    // TODO proper cost computation
+    if(mCells.empty())
+    {
+        mCost = 0;
+        return ;
+    }
+
+    // TODO check if cells of mCells path are already conquered to give true cost
+
     // use set to ignore repeated cells
     const std::unordered_set<unsigned int> cells(mCells.begin(), mCells.end());
-    mCost = (cells.size() - 1) * 0.5f;
+
+    mCost = (mCells.size() - 1) * mUnit->GetEnergyForActionStep(MOVE) +
+            cells.size() * mUnit->GetEnergyForActionStep(CONQUER_CELL);
 }
 
 bool ConquerPath::Fail()
