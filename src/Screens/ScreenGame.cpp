3293b8fd307f4e1abbb7a8b51e1fd04fa9fba32f
Davide Coppola <vivaladav@gmail.com>
2026-08-24 23:07:57 +0200

    Fix camera disabled after loading

--- src/Screens/ScreenGame.cpp
+++ src/Screens/ScreenGame.cpp
@@ -241,10 +241,6 @@ bool ScreenGame::Load(sgl::utilities::BinaryFile & bf)
     mAllowSelection = bf.ReadBool();
     mLocalTurnInitDone = bf.ReadBool();
 
-    // camera controller
-    const bool cameraEnabled = bf.ReadBool();
-    mCamController->SetEnabled(cameraEnabled);
-
     // camera position
     mCameraInitPos.x = bf.ReadInt();
     mCameraInitPos.y = bf.ReadInt();
@@ -276,9 +272,6 @@ bool ScreenGame::Save(sgl::utilities::BinaryFile & bf) const
 
     bf.WriteBool(mAllowSelection);
     bf.WriteBool(mLocalTurnInitDone);
-
-    // camera controller
-    bf.WriteBool(mCamController->IsEnabled());
 
     // save camera position
     auto cam = mCamController->GetCamera();
