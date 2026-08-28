64fb8e8ff9e3d17e8a538407a7229ccc8852958c
Davide Coppola <vivaladav@gmail.com>
2026-08-28 15:14:33 +0200

    Bump version to 0.6.9

--- src/Screens/ScreenMainMenu.cpp
+++ src/Screens/ScreenMainMenu.cpp
@@ -357,7 +357,7 @@ void ScreenMainMenu::CreateChangelog()
     const int contX = 0;
     int contY = 0;
 
-    auto title = new sgui::Label("0.6.8 - \"GWB Game Awards build\"", font, content);
+    auto title = new sgui::Label("0.6.9 - \"Graduation battle\"", font, content);
     title->SetPosition(contX, contY);
     title->SetColor(colorContent);
 
